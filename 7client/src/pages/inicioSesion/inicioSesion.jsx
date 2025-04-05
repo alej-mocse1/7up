15a549fc61e09208fb1035a1658145b8b4bbe44d
alej Mocse <alejandromocse01@gmail.com>
2025-04-05 15:48:48 -0300

    seccion registro v2

--- 7client/src/pages/inicioSesion/inicioSesion.jsx
+++ 7client/src/pages/inicioSesion/inicioSesion.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./InicioSesion.module.css";
+import styles from "./inicioSesion.module.css";
 import Navbar from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
 import iniImg from "../../assets/titSes.png"
