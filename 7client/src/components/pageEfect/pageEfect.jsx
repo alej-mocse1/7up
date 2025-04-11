097fb384aadda64eac1fa26b3460da77a6c0be5a
alej-mocse1 <alejandromocse01@gmail.com>
2025-04-11 13:29:06 -0300

    animacion cortina

---
+++ 7client/src/components/pageEfect/pageEfect.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./PageEntrance.css"; // Agrega los estilos aquí
+
+const PageEntrance = ({ children }) => {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsAnimating(false);
+    }, 1500); // Duración de la animación
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="page-container">
+      {isAnimating && (
+        <>
+          <div className="curtain left"></div>
+            <div className="imgCentral">
+              <h1>soy la imagen</h1>
+            </div>
+          <div className="curtain right"></div>
+        </>
+      )}
+      <div className={`page-content ${isAnimating ? "hidden" : "visible"}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default PageEntrance;
